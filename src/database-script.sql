0e8c68f5f1343a0df4163ed1d45bb4e360848fbd
rkimera <rkimera2013@gmail.com>
2022-03-24 11:49:03 +0300

    add sql file to create database objects

---
+++ src/database-script.sql
@@ -0,0 +1,13 @@
+create schema IF NOT EXISTS data_migration;  
+
+CREATE TABLE IF NOT EXISTS   data_migration.districts
+(
+    id serial,
+    district_name character varying(255) NOT NULL,
+    created_at timestamp(0) without time zone DEFAULT Now(),
+    updated_at timestamp(0) without time zone DEFAULT Now() ,
+    CONSTRAINT districts_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO data_migration.districts(district_name)
+	VALUES ('Lira'),('Masaka'),('Kampala');
